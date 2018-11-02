763686ea7f867aad29b23cd8441890f40ff767db
Chris Godber <chrispgodber@gmail.com>
2018-11-02 19:02:54 +0000

    state updated for word and letters array function working
    updated app.js to do basic form func

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,49 +2,71 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import three from 'three';
-import lodash from 'lodash';
+import _ from 'lodash';
 
-let Word = '';
-let Letter = '';
-let Letters = [];
+// const Word = '';
+// let Letter = '';
+// let Letters = [];
 
-let WordArray ='';
-let  WordArraySize = WordArray.length;
+// let WordArray ='';
+// let  WordArraySize = WordArray.length;
 
 class App extends Component {
 
   constructor(props) {
 
     super(props);
-    this.state = {value: ''};
+    this.state = {
+      value: '',
+      Word: '',
+      data:[]
+    };
 
     this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    // this.handleSubmit = this.handleSubmit.bind(this);
+    this.returnLetters = this.returnLetters.bind(this);
   }
 
   handleChange(event) {
-    this.setState({value: event.target.value});
+
+    this.setState({Word: event.target.value});
+    console.log("Values Entered");
   }
 
-  handleSubmit(event) {
-    alert('A Value was submitted: ' + this.state.value);
-    Letters._.chunk(WordArray, [WordArraySize]);
-    console.log('Function splite word letters rans');
-    console.log(Letters);
-    event.preventDefault();
-  }
+  // handleSubmit(event) {
+  //   alert('New Charecter entered: ' + this.state.Word);
+  //   event.preventDefault();
+  //   console.log("Input Submitted");
+  //   console.log("Hanldesubmit-state is", this.state);
+ 
+  // }
+
+  returnLetters(e){
+    e.preventDefault();
+    var rawData =  this.state.Word;
+    var RawDataLength = rawData.length;
+    var Letters =  _.chunk(rawData,RawDataLength);
+    console.log("Letters to output - "+ Letters+rawData,RawDataLength);
+   
+     this.setState(
+      {returnLetters : Letters},
+      console.log("Letters to output - "+ Letters)
+     );
+  };
 
   render() {
+ 
     return (
+
       <div className="App">
         <header className="App-header">
           <h1>WordCrunch 3D</h1>
           <p>Add a word or phrase into the form below and then press transform to trasnform the letters into a 3D Object</p>
           <div className="form-group">
-         <form onSubmit={this.handleSubmit}>
+         <form onSubmit={this.returnLetters}>
         <label>
           Name:
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
+          <input type="text" value={this.state.Word} onChange={this.handleChange} />
         </label>
         <input type="submit" value="Submit" />
       </form>
